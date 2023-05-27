00f79c45c2ef2b6b261676be4d3d1c9b0c8722f5
OscarVM117 <oscarvemo@unisabana.edu.co>
2023-05-26 21:17:01 -0500

    Create the forms front with functionality Js

--- src/Componentes/FormularioPracticas.jsx
+++ src/Componentes/FormularioPracticas.jsx
@@ -1,19 +1,39 @@
-export const FormularioPracticas=()=>{
-
-    const Formulario=()=>{
-        return(
+export const FormularioPracticas = () => {
+const estiloboton={
+    margin_bottom:'18px',
+}
+const estiloaltura={
+    height:'108px',
+}
+    const Formulario = () => {
+        return (
             <>
-            
-            {
-            //Aca tiene que ir todo el formulario de practicas
-            }
+            <div class="wrapper">
+                <form action="">
+                    <div id="wizard">     
+                        <h4></h4>
+                        <section>
+                            <div class="form-row"> <input type="text" class="form-control" placeholder="Name"/> </div>
+                            <div class="form-row"> <input type="text" class="form-control" placeholder="Email"/> </div>
+                            <div class="form-row"> <input type="text" class="form-control" placeholder="Phone number"/> </div>
+                            <div class="form-row"> <input type="text" class="form-control" placeholder="Street address"/> </div>
+                        </section> 
+                        <h4></h4>
+                        <section>
+                            <div class="form-row"> <input type="text" class="form-control" placeholder="country"/> </div>
+                            <div class="form-row"> <input type="text" class="form-control" placeholder="zip code"/> </div>
+                            <div class="form-row" style= {estiloboton}> <textarea name="" id="" class="form-control" placeholder="Any order note about delivery or special offer"  style= {estiloaltura}></textarea></div>
+                        </section>
+                    </div>
+                </form>    
+            </div>            
             </>
         )
     }
-    return(
+    return (
         <>
-        {Formulario()}
-        
+            {Formulario()}
+
         </>
     )
 }
