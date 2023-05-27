76c5f3a9592004c8f18dbdcd542511e9f5785bb7
OscarVM117 <oscarvemo@unisabana.edu.co>
2023-05-27 18:44:22 -0500

    Create draft two of table

--- src/Componentes/MostrarPracticas.jsx
+++ src/Componentes/MostrarPracticas.jsx
@@ -2,28 +2,36 @@ import { Link } from 'react-router-dom'
 export const MostrarPracticas = ()=>{
     return(              
 <>
-        <table className="table">
-            <thead>
-            <tr>
-                <th scope="col">Id estudiante</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Semestre</th>
-                <th scope="col">Facultad</th>
-                <th scope="col">Genero</th>
-                <th scope="col">Programa</th>
-                <th scope="col">Acciones</th>
-            
-            </tr>
-            </thead>
-        </table>
-
-
-
-
-
-
-
-
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
 
         
            <Link to="/"><button onClick={()=> window.close()}>Volver</button></Link>
