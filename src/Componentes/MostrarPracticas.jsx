4a703dd1546323e29b2ef598c15734aa9c9dc6bd
OscarVM117 <oscarvemo@unisabana.edu.co>
2023-05-27 18:36:24 -0500

    Create draft table

--- src/Componentes/MostrarPracticas.jsx
+++ src/Componentes/MostrarPracticas.jsx
@@ -1,8 +1,32 @@
 import { Link } from 'react-router-dom'
 export const MostrarPracticas = ()=>{
-    return(
-        <>
-            <Link to="/"><button onClick={()=> window.close()}>Volver</button></Link>
+    return(              
+<>
+        <table className="table">
+            <thead>
+            <tr>
+                <th scope="col">Id estudiante</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Semestre</th>
+                <th scope="col">Facultad</th>
+                <th scope="col">Genero</th>
+                <th scope="col">Programa</th>
+                <th scope="col">Acciones</th>
+            
+            </tr>
+            </thead>
+        </table>
+
+
+
+
+
+
+
+
+
+        
+           <Link to="/"><button onClick={()=> window.close()}>Volver</button></Link>
         </>
     )
 }
