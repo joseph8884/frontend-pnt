a5893b0b9d8f550e4687041254dd6fb41a41a31e
joseph8884 <josegzm08@gmail.com>
2023-05-26 21:15:14 -0500

    Declarate variables for the form

--- src/Componentes/FormularioPracticas.jsx
+++ src/Componentes/FormularioPracticas.jsx
@@ -1,4 +1,84 @@
-export const FormularioPracticas=()=>{
+import { useState } from "react"
+export const FormularioPracticas=({practicas, setPracticas})=>{
+
+        //useState para Empresa
+        const [nombreEmpresa, setNombreEmpresa]= useState("")
+        const [sitioWebEmpresa, setSitioWebEmpresa]=useState("")
+        const [direccionEmpresa, setDireccionEmpresa]=useState("")
+        const [numeroTelefonicoEmpresa, setNumeroTelefonicoEmpresa]=useState("")
+        const [correoElectronicoEmpresa, setCorreoElectronicoEmpresa]=useState("")
+        const [descripcionEmpresa, setDescripcionEmpresa]=useState("")
+    
+        //useState para Supervisor
+        const[nombreCompletoSupervisor, setNombreCompletoSupervisor]=useState("")
+        const[cargoSupervisor, setNombreSupervisor]=useState("")
+        const[telefonoSupervisor, setTelefonoSupervisor]=useState("")
+    
+        //useState Tareas
+        const [descripcionTarea, setDescripcionTarea]=useState("")
+        const [importanciaTarea, setImportanciaTarea] = useState("")
+        const [fechaRealizacionTarea, setFechaRealizacionTarea] = useState("")
+        const [horasDedicadaTarea , sethorasDedicadaTarea] = useState("")
+    
+    const guardarPractica=(event)=>{
+        if (nombreEmpresa === ""){
+            return alert("INGRESE UN VALOR EN NOMBRE")
+        }
+        if (sitioWebEmpresa===""){
+            return alert("INGRESE UN VALOR EN SITIOWEB")
+        }
+        if (direccionEmpresa ===""){
+            return alert("INGRESE UN VALOR EN DIRECCION")
+        }
+        if (numeroTelefonicoEmpresa===""){
+            return alert("INGRESE UN VALOR EN NUMERO")
+        }
+        if (correoElectronicoEmpresa === ""){
+            return alert("INGRESE UN VALOR EN CORREOELECTRONICO")
+        }
+        if (descripcionEmpresa === ""){
+            return alert("INGRESE UN VALOR EN DESCRIPCIONEMPRESA")
+        }
+        if (nombreCompletoSupervisor ===""){
+            return alert("INGRESE UN VALOR EN EL NOMBRE DEL SUPERVISOR")
+        }
+        if (cargoSupervisor ===""){
+            return alert("INGRESE UN VALOR EN CARGO SUPERVISOR")
+        }
+        if (telefonoSupervisor ===""){
+            return alert("INGRESE UN VALOR EN TELEFONO SUPERVISOR")
+        }
+        if (descripcionTarea ===""){
+            return alert("INGRESE UN VALOR EN DESCRIPCION TAREA")
+        }
+        if (importanciaTarea ===""){
+            return alert("INGRESE UN VALOR EN IMPORTANCIA TAREA")
+        }
+        if (fechaRealizacionTarea===""){
+            return alert("INGRESE UN VALOR EN FECHA REALIZACION TAREA")
+        }
+        if (horasDedicadaTarea ===""){
+            return alert("INGRESE UN VALOR EN HORAS DEDICADA A TAREA")
+        }
+        let practica={
+            nombreEmpresa: nombreEmpresa,
+            sitioWebEmpresa: sitioWebEmpresa,
+            direccionEmpresa: direccionEmpresa,
+            numeroTelefonicoEmpresa: numeroTelefonicoEmpresa,
+            correoElectronicoEmpresa: correoElectronicoEmpresa,
+            descripcionEmpresa:descripcionEmpresa,
+
+            nombreCompletoSupervisor:nombreCompletoSupervisor,
+            cargoSupervisor:cargoSupervisor,
+            telefonoSupervisor:telefonoSupervisor,
+
+            descripcionTarea:descripcionTarea,
+            importanciaTarea:importanciaTarea,
+            fechaRealizacionTarea:fechaRealizacionTarea,
+            horasDedicadaTarea:horasDedicadaTarea,
+        }
+        
+    }
 
     const Formulario=()=>{
         return(
@@ -13,7 +93,6 @@ export const FormularioPracticas=()=>{
     return(
         <>
         {Formulario()}
-        
         </>
     )
 }
--- src/Practicas.jsx
+++ src/Practicas.jsx
@@ -1,11 +1,17 @@
+
+import { useState } from "react"
 import { FormularioPracticas } from "./Componentes/FormularioPracticas"
 
 export const Practicas=()=>{
-
-
+         //useState Practicas
+        const [practicas, setPracticas]=useState([])
     return(
         <>
-        <FormularioPracticas></FormularioPracticas>
+        <FormularioPracticas
+        practicas={practicas}
+        setPracticas={(practica)=>setPracticas([...practicas,practica])}
+        />
+        
         {
             //Componente de Formulario
             //Boton que lleva a buscar. 
