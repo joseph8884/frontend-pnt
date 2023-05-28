a654e5f39e31025557809a89a1da04bcbfbf24eb
Santiago Santafe <andressasi@unisabana.edu.co>
2023-05-28 15:26:48 -0500

    Modify postPracticas to parse Json

--- src/Peticiones/postPracticas.js
+++ src/Peticiones/postPracticas.js
@@ -1,8 +1,18 @@
 export const postPracticas = async (practica) => {
-    const url = 'http://localhost:8080/practicas/agregar';
-    const resp = await fetch(url, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' }, 
-      body: JSON.stringify(practica)
-    });
-  };
+  const url = 'http://localhost:8080/practicas/agregar';
+  const resp = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(practica)
+  });
+
+  if (resp.ok) {
+    // La solicitud fue exitosa (código de estado 2xx)
+    console.log('La práctica se ha agregado correctamente');
+    // Aquí puedes realizar acciones adicionales si es necesario
+  } else {
+    // La solicitud no fue exitosa (código de estado diferente de 2xx)
+    console.error('Error al agregar la práctica');
+    // Aquí puedes manejar el error o mostrar un mensaje de error al usuario
+  }
+};
