f464ebc907c62e4ef5b3ff1953e0a40c70b3948e
joseph8884 <josegzm08@gmail.com>
2023-05-28 15:08:38 -0500

    mistake in post request

--- src/Peticiones/postPracticas.js
+++ src/Peticiones/postPracticas.js
@@ -1,8 +1,8 @@
-export const postPracticas = async(practica) => {
-    const url='http://localhost:8080/practicas/agregar';
-    const resp=await fetch (url,{
-        method:'POST',
-        headers:{'Content-type':'appliaction/json'},
-        body:JSON.stringify(practica)
-    })
-}
+export const postPracticas = async (practica) => {
+    const url = 'http://localhost:8080/practicas/agregar';
+    const resp = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' }, 
+      body: JSON.stringify(practica)
+    });
+  };
