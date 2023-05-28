994c1cb3b93ec827f92a172405c841085b4fc9a4
Santiago Santafe <andressasi@unisabana.edu.co>
2023-05-27 22:47:04 -0500

    Change principal menu h1

--- src/Componentes/MenuPrincipal.jsx
+++ src/Componentes/MenuPrincipal.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const MenuPrincipal = () => {
   return (
     <>
-        <h1>Bienvenido a la pagina de navegación</h1>
+        <h1>Bienvenido a la pagina de navegación para tus Practicas Profesionales!</h1>
         <Link to="/registrarPracticas"><button>Registrar Practicas</button></Link>
         <Link to="/consultarPracticas"><button>Consultar Practicas</button></Link>
         {/*poner el atributo target='_blank' para cuando se contecte con el back*/}
